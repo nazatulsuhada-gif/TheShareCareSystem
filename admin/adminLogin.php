f87f69e0d975908b8e485f08741fa043dfe71191
nurin-rahzani4 <nurinrahzani907@gmail.com>
2026-06-25 19:03:31 +0800

    change header using head.php

--- admin/adminLogin.php
+++ admin/adminLogin.php
@@ -131,18 +131,7 @@ input{
 
 <body>
 
-<div class="header">
-    <a href="role.php">
-        <img src="../images/back.png" class="icon-img" alt="Back" />
-    </a>
-
-    <div class="logo">⚪ THE SHARE CARE</div>
-
-    <a href="index.php">
-        <img src="../images/home.png" class="icon-img" alt="Home" />
-    </a>
-    
-</div>
+<?php include("head.php"); ?>
 
 <div class="container">
 
