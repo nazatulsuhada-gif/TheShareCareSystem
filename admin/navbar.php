72217c776a04e8074b6f63ba30522aa843729d60
nurin-rahzani4 <nurinrahzani907@gmail.com>
2026-06-25 19:07:51 +0800

    navigation bar

---
+++ admin/navbar.php
@@ -0,0 +1,16 @@
+<nav class="admin-nav">
+    <div class="nav-brand">
+        <span class="brand-icon">⚪</span>
+        THE SHARE CARE
+        <span class="badge">admin</span>
+    </div>
+
+    <ul class="nav-links">
+        <li><a href="adminDashboard.php">Dashboard</a></li>
+        <li><a href="adminDonationReview.php">Donation</a></li>
+        
+        <li><a href="adminRequestManagement.php">Requests</a></li>
+
+        <li><a href="logout.php" class="logout-link">Logout</a></li>
+    </ul>
+</nav>
