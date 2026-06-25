f765ee2732697e629ad2bb8d8350d8c81f8c1d3a
nurin-rahzani4 <nurinrahzani907@gmail.com>
2026-06-25 22:53:56 +0800

    rename placeholder search

--- admin/search.php
+++ admin/search.php
@@ -7,7 +7,7 @@
 </head>
 <body>
     <form method="GET">
-    <input type="text" name="search" placeholder="Search User">
+    <input type="text" name="search" placeholder="Search">
     <button type="submit">Search</button>
 </form>
 </body>
