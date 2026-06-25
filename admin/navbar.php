5f79cd49bc3727f21aa74878ba8042a3b2bddc73
nurin-rahzani4 <nurinrahzani907@gmail.com>
2026-06-26 04:11:36 +0800

    add nav for messsage

--- admin/navbar.php
+++ admin/navbar.php
@@ -10,7 +10,7 @@
         <li><a href="adminDonationReview.php">Donation</a></li>
         <li><a href="adminRequestManagement.php">Requests</a></li>
         <li><a href="adminReportManagement.php">Report</a></li>
-        <li><a href="adminMessageManagement.php">Messages</a></li>
+        <li><a href="adminMessageRead.php">Messages</a></li>
 
         <li><a href="logout.php" class="logout-link">Logout</a></li>
     </ul>
