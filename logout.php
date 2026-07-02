a72fa4156d4bc1be5f9bb9034ae4cf442c094e27
nurin-rahzani4 <nurinrahzani907@gmail.com>
2026-07-02 18:10:55 +0800

    Update logout.php

--- logout.php
+++ logout.php
@@ -1,14 +1,15 @@
 <?php
 session_start();
-$_SESSION = array(); 
-if (ini_get("session.use_cookies")) {
-    $params = session_get_cookie_params();
-    setcookie(session_name(), '', time() - 42000,
-        $params["path"], $params["domain"],
-        $params["secure"], $params["httponly"]
-    );
+
+$isAdmin = isset($_SESSION['admin']);
+
+$_SESSION = array();
+session_destroy();
+
+if ($isAdmin) {
+    header("Location: adminLogin.php");
+} else {
+    header("Location: login.php");
 }
-session_destroy(); 
-header("Location: login.php");
 exit();
 ?>
