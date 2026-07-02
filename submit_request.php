f21591d19efc50fdb4edc401d611329a8d18f747
nurin-rahzani4 <nurinrahzani907@gmail.com>
2026-07-02 18:05:25 +0800

    Update submit_request.php

--- submit_request.php
+++ submit_request.php
@@ -11,7 +11,7 @@ $donation_id = $_GET['donation_id'] ?? '';
 $student_id = $_SESSION['user_id']; 
 
 if (empty($donation_id)) {
-    echo "<script>alert('Donation id not found.'); window.history.back();</script>";
+    echo "<script>alert('Donation ID not provided.'); window.history.back();</script>";
     exit();
 }
 
@@ -22,7 +22,7 @@ $stmt_check->execute();
 $result_check = $stmt_check->get_result();
 
 if ($result_check->num_rows === 0) {
-    echo "<script>alert('Error: this item is no longer available or not found'); window.history.back();</script>";
+    echo "<script>alert('Error: This item is no longer available or not found.'); window.history.back();</script>";
     exit();
 }
 
@@ -34,6 +34,6 @@ try {
         echo "<script>alert('Request submitted successfully!'); window.location.href='request.php';</script>";
     }
 } catch (mysqli_sql_exception $e) {
-    echo "<script>alert('Error : Request submission failed'); window.history.back();</script>";
+    echo "<script>alert('Error: Request submission failed!'); window.history.back();</script>";
 }
 ?>
